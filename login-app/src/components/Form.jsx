f1671e74c399ade1ed5399377f699cfa69d46ec7
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2021-11-29 11:35:36 -0300

    Novo Layout

--- login-app/src/components/Form.jsx
+++ login-app/src/components/Form.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { LoginIcon, LockClosedIcon } from '@heroicons/react/outline';
 
-const styleInput = `border-b-2 border-indigo-400
+const styleInput = `border-b-2 border-green-400
   flex focus:outline-none
   font-bold mb-2 p-3
-  placeholder-indigo-700
+  placeholder-green-700
   placeholder-opacity-25
   text-center text-gray-700
   text-sm tracking-wide w-5/6`;
@@ -14,13 +14,13 @@ export default class Form extends Component {
     return (
       <>
         <form className="flex flex-col items-center w-full max-w-lg">
-            <LoginIcon className="relative -bottom-11 -left-44 w-8 text-indigo-400" />
+            <LoginIcon className="relative -bottom-11 -left-40 w-10 text-green-400" />
             <input 
               type="email"
               className= { styleInput }
               placeholder="Email/Usuário"
             />
-            <LockClosedIcon className="relative -bottom-10 -left-44 w-8 text-indigo-400" />
+            <LockClosedIcon className="relative -bottom-10 -left-40 w-10 text-green-400" />
             <input
               type="password"
               className={ styleInput }
@@ -29,12 +29,12 @@ export default class Form extends Component {
             <button
               className="bg-gradient-to-r
                 flex focus:ring font-semibold
-                from-blue-700 hover:border-blue-400
-                hover:from-blue-300 hover:text-gray-500
+                from-green-700 hover:border-green-400
+                hover:from-green-300 hover:text-gray-800
                 justify-center mt-2 p-2
                 ring-indigo-200 ring-offset-2
-                rounded-sm text-opacity-80
-                text-white to-blue-400 w-4/6"
+                rounded-sm text-white
+                to-green-400 w-4/6"
             >
               Entrar
             </button>
