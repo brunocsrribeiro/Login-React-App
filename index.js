4a8d261c97dd10e57b5c253423825ebef1a3578e
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-05-09 11:23:21 -0300

    NEW: Second commit: nova messagem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || null;
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.status(200).end();
+  return res.status(200).send({ message: 'Testando o Heroku' });
 });
 
 app.listen(PORT, () => {
