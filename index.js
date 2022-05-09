2c380e0214a7836430ed221428a0ef9eec532b2c
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-05-09 11:45:24 -0300

    NEW: Third commit: criando branch de porducao e homologacao

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const PORT = process.env.PORT || null;
+const SERVER_ENV = process.env.SERVER_ENV || null
 
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.status(200).send({ message: 'Testando o Heroku' });
+  if (SERVER_ENV) {
+    return res.status(200)
+      .send({ message: `Dummy endpoint running on enviroment ${SERVER_ENV}` });
+  }
+
+  return res.status(200).send({ message: 'Dummy endpoint' });
 });
 
 app.listen(PORT, () => {
